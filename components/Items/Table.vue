8c0ddc5beb5ef2000cc3c1acd08d93fc89ed6d2a
diespeso <regalado.correo@gmail.com>
2024-01-25 22:50:06 -0800

    edit view working well, crud for frontend completed

--- components/Items/Table.vue
+++ components/Items/Table.vue
@@ -14,13 +14,27 @@
         </thead>
         <tbody>
             <tr v-for="item in items.data" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.description }}</td>
-                <td>{{ item.price }}</td>
+                <td>
+                    <span >{{ item.id }}</span>
+                </td>
+                <td>
+                    <v-text-field v-if="currentEdit && currentEdit == item.id" v-model="item.name" />
+                    <span v-else>{{ item.name }}</span>
+                </td>
+                <td>
+                    <v-text-field v-if="currentEdit && currentEdit == item.id" v-model="item.description"/>
+                    <span v-else>{{ item.description }}</span>
+                </td>
+                <td>
+                    <v-text-field v-if="currentEdit && currentEdit == item.id" v-model="item.price"/>
+                    <span v-else>{{ item.price }}</span>
+                </td>
                 <td>
                     <div>
-                        <v-btn v-on:click="controls.onClickDelete(item.id)">x</v-btn>
+                        <v-btn v-on:click="controls.onClickDelete(item.id!)">x</v-btn>
+                        <v-btn v-if="!currentEdit || currentEdit != item.id" v-on:click="controls.onClickEdit(item.id!)" >Edit</v-btn>
+                        <v-btn v-show="currentEdit == item.id" v-on:click="controls.onSaveEdit(item)">Save</v-btn>
+                        <v-btn v-show="currentEdit == item.id" v-on:click="currentEdit = null">Cancel</v-btn>
                     </div>
                 </td>
             </tr>
@@ -35,13 +49,18 @@
         items: Item[]
     }>()
 
+    const currentEdit: Ref<number | null> = useState('currentEdit', () => null)
+
     const columns = [{
         key: 'id',
         label: 'ID'
     }]
-    const { getItems, getItemById, deleteItemById } = useDbItems()
 
-    const items = reactive({
+    const { getItems, deleteItemById, updateItemById } = useDbItems()
+
+    const items: {
+        data: Item[]
+    } = reactive({
         data: []
     })
 
@@ -54,10 +73,31 @@
 
     const controls = {
         onClickDelete : async (id: number) => {
-            const { success }= await deleteItemById(id)
+            const { success, message}= await deleteItemById(id)
 
             if (success) {
                 items.data = items.data.filter(item => item.id != id)
+            } else {
+                alert(message)
+            }
+        },
+        onClickEdit: async (id: number) => {
+            if (currentEdit.value) {
+                if (currentEdit.value == id) {
+                    currentEdit.value = null // toggle
+                }
+            } else {
+                currentEdit.value = id;
+            }
+        },
+        onSaveEdit: async (item: Item) => {
+            const { success, data: updated, message} = await updateItemById(item.id!, item)
+
+            if (success) {
+                items.data = items.data.map(_item => (_item.id == updated?.id ? updated : _item))
+                currentEdit.value = null
+            } else {
+                alert(message)
             }
         }
     }
---
+++ prisma/migrations/20240126063045_fixed_timestamps/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Item" ALTER COLUMN "created_at" SET DEFAULT CURRENT_TIMESTAMP;
